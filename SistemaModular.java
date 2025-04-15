0d6dd384f530dd349d3452430b19fcfe09915372
GIOVANI LENOIR <giovani_lenoir@estudante.sesisenai.org.br>
2025-04-14 21:24:45 -0300

    Sistema Modular - Em Produção

--- SistemaModular.java
+++ SistemaModular.java
@@ -2,14 +2,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SistemaModular {
-    static Scanner sc = new Scanner(System.in);
-    static ArrayList<String> logInfo = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<String> EPIs = new ArrayList<>();
+    static ArrayList<String> Funcionarios = new ArrayList<>();
+    static ArrayList<String> Emprestimos = new ArrayList<>();
+    static ArrayList<String> logOperacoes = new ArrayList<>();
 
     public static void main(String[] args) {
-        ArrayList<String> EPIs = new ArrayList<>();
-        ArrayList<String> Funcionarios = new ArrayList<>();
-        ArrayList<String> Emprestimos = new ArrayList<>();
-
         int opcao;
         do {
             System.out.println("-----Sistema de Gerenciamento-----");
@@ -18,29 +17,32 @@ public class SistemaModular {
             System.out.println("3. --Módulo de Empréstimos--");
             System.out.println("0. Sair ");
             System.out.println("Escolha uma Opção: ");
-            int opcao = crudEPI.input.nextInt();
-            crudEPI.input.nextLine();
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    SubMenu(crudEPI, crudEPI.epis);
-                break;
+                    new SubMenu(EPIs, crudEPI.epis);
+                    break;
                 case 2:
-                    SubMenu(usuarioEPI, usuarioEPI.Usuarios);
-                break;
+                    new SubMenu(Funcionarios, usuarioEPI.Usuarios);
+                    break;
                 case 3:
-                    SubMenu(emprestimoEPI, emprestimoEPI.EmprestimoEPI);
-                break;
-                case 0:
+                    new SubMenu(Emprestimos, emprestimoEPI.EmprestimoEPI);
+                    break;
+                case 0 -> {
                     System.out.println("Encerranso Sistema...");
-                break;
-                default:
-                    System.out.println("Opção Inválida.");
+                    System.out.println("---Log de Operações---");
+                    for (String log : logOperacoes) {
+                        System.out.println(log);
+                    }
+                }
+                default -> System.out.println("Opção Inválida.");
+
             }
-        }while (opcao != 0);
+        } while (opcao != 0);
     }
-
-
 }
+
 
 
--- SubMenu.java
+++ SubMenu.java
@@ -1,3 +1,33 @@
-public class SubMenu {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class SubMenu {
+    static Scanner scanner = new Scanner(System.in);
+
+    public SubMenu(ArrayList<String> Lista, String nomeModulo {
+        int opcao;
+        do {
+            System.out.println("\n ----Modulos " + nomeModulo + "----");
+            System.out.println("1. --Cadastrar--");
+            System.out.println("2. --Listar--");
+            System.out.println("3. --Atualizar--");
+            System.out.println("4. --Remover--");
+            System.out.println("5. --Voltar--");
+            System.out.println("----Escolha uma Opção----");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1 -> {
+                    System.out.println("Digite seu nome: ");
+                    String entrada = scanner.nextLine();
+                    if (!entrada.isBlank()) ;
+                    Lista.add(entrada);
+                    System.out.println("Cadastro realizado com sucesso!");
+                    SistemaModular.logOperacoes.add("[" + nomeModulo + " Cadastro: " + entrada);
+                }
+            }
+        }
+    }
 }
+
